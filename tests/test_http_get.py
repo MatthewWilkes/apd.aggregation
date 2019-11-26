eb11be2a4e3816d86f9651ba6d8b1e7060e6caff
Matthew Wilkes <git@matthewwilkes.name>
2019-11-26 13:33:07 +0000

    Convert tests to asyncio plugin

--- tests/test_http_get.py
+++ tests/test_http_get.py
@@ -1,4 +1,3 @@
-import asyncio
 from concurrent.futures import ThreadPoolExecutor
 import datetime
 from unittest.mock import patch, MagicMock
@@ -36,6 +35,7 @@ def get_independent_flask_app(name):
 def run_server_in_thread(name, config, port):
     # Keep a shared reference to allow the main thread to stop the worker thread
     running = True
+    base = f"http://localhost:{port}/"
 
     def set_up_and_serve():
         app = get_independent_flask_app(
@@ -48,10 +48,10 @@ def run_server_in_thread(name, config, port):
 
     with ThreadPoolExecutor() as pool:
         pool.submit(set_up_and_serve)
-        yield
+        yield base
         running = False
         # Make a final request, to cause the loop to re-evaluate the running condition
-        requests.get(f"http://localhost:{port}")
+        requests.get(base)
 
 
 @pytest.fixture(scope="module")
@@ -73,16 +73,14 @@ class TestGetDataPoints:
     def mut(self):
         return collect.get_data_points
 
-    def test_get_data_points(self, sensors, mut, http_server):
-        async def wrapped():
-            async with aiohttp.ClientSession() as http:
-                return await mut("http://localhost:12081/", "testing", http)
-
+    @pytest.mark.asyncio
+    async def test_get_data_points(self, sensors, mut, http_server):
         # Get the data from the server, storing the time before and after
         # as bounds for the collected_at value
-        time_before = datetime.datetime.now()
-        results = asyncio.run(wrapped())
-        time_after = datetime.datetime.now()
+        async with aiohttp.ClientSession() as http:
+            time_before = datetime.datetime.now()
+            results = await mut(http_server, "testing", http)
+            time_after = datetime.datetime.now()
 
         assert len(results) == len(sensors) == 2
 
@@ -91,16 +89,16 @@ class TestGetDataPoints:
             assert result.sensor_name == sensor.name
             assert time_before <= result.collected_at <= time_after
 
-    def test_get_data_points_fails_with_bad_api_key(self, sensors, mut, http_server):
-        async def wrapped():
-            async with aiohttp.ClientSession() as http:
-                return await mut("http://localhost:12081/", "incorrect", http)
-
+    @pytest.mark.asyncio
+    async def test_get_data_points_fails_with_bad_api_key(
+        self, sensors, mut, http_server
+    ):
         with pytest.raises(
             ValueError,
-            match="Error loading data from http://localhost:12081/: Supply API key in X-API-Key header",
+            match=f"Error loading data from {http_server}: Supply API key in X-API-Key header",
         ):
-            asyncio.run(wrapped())
+            async with aiohttp.ClientSession() as http:
+                await mut(http_server, "incorrect", http)
 
 
 class TestAddDataFromSensors:
@@ -112,43 +110,31 @@ class TestAddDataFromSensors:
     def mock_db_session(self):
         return MagicMock()
 
-    def test_get_get_data_from_sensors(
+    @pytest.mark.asyncio
+    async def test_get_get_data_from_sensors(
         self, mock_db_session, sensors, mut, http_server
     ):
-        async def wrapped():
-            return await mut(mock_db_session, ["http://localhost:12081/"], "testing")
-
-        results = asyncio.run(wrapped())
+        results = await mut(mock_db_session, [http_server], "testing")
         assert mock_db_session.add.call_count == len(sensors)
         assert len(results) == len(sensors)
 
-    def test_get_get_data_from_sensors_with_multiple_servers(
+    @pytest.mark.asyncio
+    async def test_get_get_data_from_sensors_with_multiple_servers(
         self, mock_db_session, sensors, mut, http_server
     ):
-        async def wrapped():
-            return await mut(
-                mock_db_session,
-                ["http://localhost:12081/", "http://localhost:12081/"],
-                "testing",
-            )
-
-        results = asyncio.run(wrapped())
+        results = await mut(mock_db_session, [http_server, http_server], "testing")
         assert mock_db_session.add.call_count == len(sensors) * 2
         assert len(results) == len(sensors) * 2
 
-    def test_data_points_not_added_if_only_partial_success(
+    @pytest.mark.asyncio
+    async def test_data_points_not_added_if_only_partial_success(
         self, mock_db_session, sensors, mut, http_server, bad_api_key_http_server
     ):
-        async def wrapped():
-            return await mut(
-                mock_db_session,
-                ["http://localhost:12081/", "http://localhost:12082/"],
-                "testing",
-            )
-
         with pytest.raises(
             ValueError,
-            match="Error loading data from http://localhost:12082/: Supply API key in X-API-Key header",
+            match=f"Error loading data from {bad_api_key_http_server}: Supply API key in X-API-Key header",
         ):
-            asyncio.run(wrapped())
+            await mut(
+                mock_db_session, [http_server, bad_api_key_http_server], "testing"
+            )
         assert mock_db_session.add.call_count == 0
