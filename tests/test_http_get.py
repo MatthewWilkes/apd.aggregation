13bcd7ce05c2fc52498b6bebfe49bd96c6785356
Matthew Wilkes <git@matthewwilkes.name>
2019-11-28 13:23:48 +0000

    Add type hinting and refine scopes

--- tests/test_http_get.py
+++ tests/test_http_get.py
@@ -1,30 +1,31 @@
 from concurrent.futures import ThreadPoolExecutor
 import datetime
+import typing as t
 from unittest.mock import patch, MagicMock
 import wsgiref.simple_server
 
 import aiohttp
+from apd.sensors.base import Sensor
 from apd.sensors.sensors import PythonVersion, ACStatus
 from apd.sensors.wsgi import set_up_config
 import flask
 import pytest
-import requests
 
 from apd.sensors.wsgi import v20
 
 from apd.aggregation import collect
 
 
-@pytest.fixture(scope="module")
-def sensors():
+@pytest.fixture
+def sensors() -> t.Iterator[t.List[Sensor[t.Any]]]:
     """ Patch the get_sensors method to return a known pair of sensors only """
-    data = [PythonVersion(), ACStatus()]
+    data: t.List[Sensor[t.Any]] = [PythonVersion(), ACStatus()]
     with patch("apd.sensors.cli.get_sensors") as get_sensors:
         get_sensors.return_value = data
         yield data
 
 
-def get_independent_flask_app(name):
+def get_independent_flask_app(name: str) -> flask.Flask:
     """ Create a new flask app with the v20 API blueprint loaded, so multiple copies
     of the app can be run in parallel without conflicting configuration """
     app = flask.Flask(name)
@@ -32,37 +33,29 @@ def get_independent_flask_app(name):
     return app
 
 
-def run_server_in_thread(name, config, port):
-    # Keep a shared reference to allow the main thread to stop the worker thread
-    running = True
-    base = f"http://localhost:{port}/"
-
-    def set_up_and_serve():
-        app = get_independent_flask_app(
-            name
-        )  # Create a new flask app and load in required code, to prevent config conflicts
-        flask_app = set_up_config(config, app)
-        with wsgiref.simple_server.make_server("localhost", port, flask_app) as server:
-            while running:
-                server.handle_request()  # Process requests one-by-one to allow the main thread to stop the loop
+def run_server_in_thread(
+    name: str, config: t.Dict[str, t.Any], port: int
+) -> t.Iterator[str]:
+    # Create a new flask app and load in required code, to prevent config conflicts
+    app = get_independent_flask_app(name)
+    flask_app = set_up_config(config, app)
+    server = wsgiref.simple_server.make_server("localhost", port, flask_app)
 
     with ThreadPoolExecutor() as pool:
-        pool.submit(set_up_and_serve)
-        yield base
-        running = False
-        # Make a final request, to cause the loop to re-evaluate the running condition
-        requests.get(base)
+        pool.submit(server.serve_forever)
+        yield f"http://localhost:{port}/"
+        server.shutdown()
 
 
 @pytest.fixture(scope="module")
-def http_server(sensors):
+def http_server():
     yield from run_server_in_thread(
         "standard", {"APD_SENSORS_API_KEY": "testing"}, 12081
     )
 
 
 @pytest.fixture(scope="module")
-def bad_api_key_http_server(sensors):
+def bad_api_key_http_server():
     yield from run_server_in_thread(
         "alternate", {"APD_SENSORS_API_KEY": "penny"}, 12082
     )
@@ -73,8 +66,18 @@ class TestGetDataPoints:
     def mut(self):
         return collect.get_data_points
 
+    @pytest.fixture(scope="session")
+    def http_server(self):
+        print("Set up class")
+        yield from run_server_in_thread(
+            "standard", {"APD_SENSORS_API_KEY": "testing"}, 12081
+        )
+        print("Tear down c;ass")
+
     @pytest.mark.asyncio
-    async def test_get_data_points(self, sensors, mut, http_server):
+    async def test_get_data_points(
+        self, sensors: t.List[Sensor[t.Any]], mut, http_server: str
+    ) -> None:
         # Get the data from the server, storing the time before and after
         # as bounds for the collected_at value
         async with aiohttp.ClientSession() as http:
@@ -91,8 +94,8 @@ class TestGetDataPoints:
 
     @pytest.mark.asyncio
     async def test_get_data_points_fails_with_bad_api_key(
-        self, sensors, mut, http_server
-    ):
+        self, sensors: t.List[Sensor[t.Any]], mut, http_server: str
+    ) -> None:
         with pytest.raises(
             ValueError,
             match=f"Error loading data from {http_server}: Supply API key in X-API-Key header",
@@ -112,24 +115,29 @@ class TestAddDataFromSensors:
 
     @pytest.mark.asyncio
     async def test_get_get_data_from_sensors(
-        self, mock_db_session, sensors, mut, http_server
-    ):
+        self, mock_db_session, sensors: t.List[Sensor[t.Any]], mut, http_server: str
+    ) -> None:
         results = await mut(mock_db_session, [http_server], "testing")
         assert mock_db_session.add.call_count == len(sensors)
         assert len(results) == len(sensors)
 
     @pytest.mark.asyncio
     async def test_get_get_data_from_sensors_with_multiple_servers(
-        self, mock_db_session, sensors, mut, http_server
-    ):
+        self, mock_db_session, sensors: t.List[Sensor[t.Any]], mut, http_server: str
+    ) -> None:
         results = await mut(mock_db_session, [http_server, http_server], "testing")
         assert mock_db_session.add.call_count == len(sensors) * 2
         assert len(results) == len(sensors) * 2
 
     @pytest.mark.asyncio
     async def test_data_points_not_added_if_only_partial_success(
-        self, mock_db_session, sensors, mut, http_server, bad_api_key_http_server
-    ):
+        self,
+        mock_db_session,
+        sensors: t.List[Sensor[t.Any]],
+        mut,
+        http_server: str,
+        bad_api_key_http_server: str,
+    ) -> None:
         with pytest.raises(
             ValueError,
             match=f"Error loading data from {bad_api_key_http_server}: Supply API key in X-API-Key header",
