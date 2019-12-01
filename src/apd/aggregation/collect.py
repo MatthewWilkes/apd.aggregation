40d2d2fe14161336481075283e2941fab50bd86f
Matthew Wilkes <git@matthewwilkes.name>
2019-12-01 20:40:17 +0000

    Switch to using classic mapper style

--- src/apd/aggregation/collect.py
+++ src/apd/aggregation/collect.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from .database import DataPoint
+from .database import DataPoint, datapoint_table
 
 
 async def get_data_points(
@@ -38,11 +38,11 @@ async def get_data_points(
         )
 
 
-async def handle_result(
-    result: t.List[DataPoint], session: Session
-) -> t.List[DataPoint]:
+def handle_result(result: t.List[DataPoint], session: Session) -> t.List[DataPoint]:
     for point in result:
-        session.add(point)
+        insert = datapoint_table.insert().values(**point._asdict())
+        sql_result = session.execute(insert)
+        point.id = sql_result.inserted_primary_key[0]
     return result
 
 
@@ -52,10 +52,11 @@ async def add_data_from_sensors(
     tasks: t.List[t.Awaitable[t.List[DataPoint]]] = []
     points: t.List[DataPoint] = []
     async with aiohttp.ClientSession() as http:
-        for server in servers:
-            tasks.append(get_data_points(server, api_key, http))
-        for a in await asyncio.gather(*tasks):
-            points += await handle_result(a, session)
+        tasks = [get_data_points(server, api_key, http) for server in servers]
+        for results in await asyncio.gather(*tasks):
+            points += results
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, handle_result, points, session)
     return points
 
 
