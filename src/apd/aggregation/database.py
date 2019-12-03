c8c30f8b55e729aa46d65c2fece814437dce6116
Matthew Wilkes <git@matthewwilkes.name>
2019-12-03 14:03:01 +0000

    Add comparator for collected_on

--- src/apd/aggregation/database.py
+++ src/apd/aggregation/database.py
@@ -5,8 +5,8 @@ import datetime
 import typing as t
 
 import sqlalchemy
-import sqlalchemy.util._collections
-from sqlalchemy.dialects.postgresql import JSONB, TIMESTAMP
+from sqlalchemy.dialects.postgresql import JSONB, DATE, TIMESTAMP
+from sqlalchemy.ext.hybrid import ExprComparator, hybrid_property
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.schema import Table
 
@@ -17,10 +17,32 @@ datapoint_table = Table(
     "sensor_values",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("sensor_name", sqlalchemy.String),
-    sqlalchemy.Column("collected_at", TIMESTAMP),
+    sqlalchemy.Column("sensor_name", sqlalchemy.String, index=True),
+    sqlalchemy.Column("collected_at", TIMESTAMP, index=True),
     sqlalchemy.Column("data", JSONB),
 )
+
+
+class DateEqualComparator(ExprComparator):
+    def __init__(self, fallback_expression, raw_expression):
+        # Do not try and find update expression from parent
+        super().__init__(None, fallback_expression, None)
+        self.raw_expression = raw_expression
+
+    def __eq__(self, other):
+        """ Returns True iff on the same day as other """
+        other_date = sqlalchemy.cast(other, DATE)
+        return sqlalchemy.and_(
+            self.raw_expression >= other_date, self.raw_expression < other_date + 1,
+        )
+
+    def operate(self, op, *other, **kwargs):
+        other = [sqlalchemy.cast(date, DATE) for date in other]
+        return op(self.expression, *other, **kwargs)
+
+    def reverse_operate(self, op, other, **kwargs):
+        other = [sqlalchemy.cast(date, DATE) for date in other]
+        return op(other, self.expression, **kwargs)
 
 
 @dataclass
@@ -39,6 +61,17 @@ class DataPoint:
         if data["id"] is None:
             del data["id"]
         return data
+
+    @hybrid_property
+    def collected_on_date(self):
+        return self.collected_at.date()
+
+    @collected_on_date.comparator  # type: ignore
+    def collected_on_date(cls):
+        return DateEqualComparator(
+            fallback_expression=sqlalchemy.cast(datapoint_table.c.collected_at, DATE),
+            raw_expression=datapoint_table.c.collected_at,
+        )
 
 
 def main():
