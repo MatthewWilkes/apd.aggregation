03a87fb482dc8ff9ac6bf5d2ff45353196c0b001
Matthew Wilkes <git@matthewwilkes.name>
2019-12-15 22:34:49 +0000

    Add tests for getting list of deployments

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,7 +1,10 @@
+from uuid import UUID
+
 import pytest
 
 from apd.aggregation.query import (
     get_data,
+    get_deployment_ids,
     db_session_var,
 )
 
@@ -22,3 +25,24 @@ class TestGetData:
         db_session_var.set(db_session)
         points = [dp async for dp in mut()]
         assert len(points) == 0
+
+
+class TestGetDeployments:
+    @pytest.fixture
+    def mut(self):
+        return get_deployment_ids
+
+    @pytest.mark.asyncio
+    async def test_get_all_deployments(self, mut, db_session, populated_db):
+        db_session_var.set(db_session)
+        deployment_ids = await mut()
+        assert set(deployment_ids) == {
+            UUID("b4c68905-b1e4-4875-940e-69e5d27730fd"),
+            UUID("6e887b81-57c3-4f3b-a7e7-bad159e05e78"),
+        }
+
+    @pytest.mark.asyncio
+    async def test_empty_db(self, mut, db_session, migrated_db):
+        db_session_var.set(db_session)
+        deployment_ids = await mut()
+        assert deployment_ids == []
