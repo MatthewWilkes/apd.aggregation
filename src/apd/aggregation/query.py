eb77a4ae0c0db8fbfcaffc6e2dbe8216926b89c8
Matthew Wilkes <git@matthewwilkes.name>
2019-12-15 21:29:12 +0000

    Add more filtering on get_data

--- src/apd/aggregation/query.py
+++ src/apd/aggregation/query.py
@@ -1,7 +1,9 @@
 import asyncio
 import contextlib
 from contextvars import ContextVar
+import datetime
 import typing as t
+from uuid import UUID
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -29,10 +31,29 @@ def with_database(uri: t.Optional[str] = None) -> t.Iterator[Session]:
     Session.close()
 
 
-async def get_data() -> t.AsyncIterator[DataPoint]:
+async def get_data(
+    sensor_name: t.Optional[str] = None,
+    deployment_id: t.Optional[UUID] = None,
+    collected_before: t.Optional[datetime.datetime] = None,
+    collected_after: t.Optional[datetime.datetime] = None,
+) -> t.AsyncIterator[DataPoint]:
     db_session = db_session_var.get()
     loop = asyncio.get_running_loop()
     query = db_session.query(datapoint_table)
+    if sensor_name:
+        query = query.filter(datapoint_table.c.sensor_name == sensor_name)
+    if deployment_id:
+        query = query.filter(datapoint_table.c.deployment_id == deployment_id)
+    if collected_before:
+        query = query.filter(datapoint_table.c.collected_at < collected_before)
+    if collected_after:
+        query = query.filter(datapoint_table.c.collected_at > collected_after)
+    query = query.order_by(
+        datapoint_table.c.deployment_id,
+        datapoint_table.c.sensor_name,
+        datapoint_table.c.collected_at,
+    )
+
     rows = await loop.run_in_executor(None, query.all)
     for row in rows:
         yield DataPoint.from_sql_result(row)
