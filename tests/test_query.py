2ef5112aa7607cc57df7eabaadcffee18b2c6bd6
Matthew Wilkes <git@matthewwilkes.name>
2019-12-15 23:31:46 +0000

    Additional filter tests for get_data

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,3 +1,4 @@
+import datetime
 from uuid import UUID
 
 import pytest
@@ -15,10 +16,29 @@ class TestGetData:
         return get_data
 
     @pytest.mark.asyncio
-    async def test_iterate_over_items(self, mut, db_session, populated_db):
+    @pytest.mark.parametrize(
+        "filter,num_items_expected",
+        [
+            ({}, 9),
+            ({"sensor_name": "Test"}, 7),
+            ({"deployment_id": UUID("b4c68905-b1e4-4875-940e-69e5d27730fd")}, 5),
+            ({"collected_after": datetime.datetime(2020, 4, 1, 12, 2, 1)}, 3),
+            ({"collected_before": datetime.datetime(2020, 4, 1, 12, 2, 1)}, 4),
+            (
+                {
+                    "collected_after": datetime.datetime(2020, 4, 1, 12, 2, 1),
+                    "collected_before": datetime.datetime(2020, 4, 1, 12, 3, 5),
+                },
+                2,
+            ),
+        ],
+    )
+    async def test_iterate_over_items(
+        self, mut, db_session, populated_db, filter, num_items_expected
+    ):
         db_session_var.set(db_session)
-        points = [dp async for dp in mut()]
-        assert len(points) == 9
+        points = [dp async for dp in mut(**filter)]
+        assert len(points) == num_items_expected
 
     @pytest.mark.asyncio
     async def test_empty_db(self, mut, db_session, migrated_db):
