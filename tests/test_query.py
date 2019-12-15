0331a25bf72e9df617e431fcd96b433bb25c0d8e
Matthew Wilkes <git@matthewwilkes.name>
2019-12-15 22:24:20 +0000

    Add test for get_data on an empty db

--- tests/test_query.py
+++ tests/test_query.py
@@ -6,14 +6,19 @@ from apd.aggregation.query import (
 )
 
 
-@pytest.mark.usefixtures("populated_db")
 class TestGetData:
     @pytest.fixture
     def mut(self):
         return get_data
 
     @pytest.mark.asyncio
-    async def test_iterate_over_items(self, mut, db_session):
+    async def test_iterate_over_items(self, mut, db_session, populated_db):
         db_session_var.set(db_session)
         points = [dp async for dp in mut()]
         assert len(points) == 9
+
+    @pytest.mark.asyncio
+    async def test_empty_db(self, mut, db_session, migrated_db):
+        db_session_var.set(db_session)
+        points = [dp async for dp in mut()]
+        assert len(points) == 0
