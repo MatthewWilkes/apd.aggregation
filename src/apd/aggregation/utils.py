7bfdf70b50e9d3ef85170d8b5277a8e11716471b
Matthew Wilkes <git@matthewwilkes.name>
2020-01-06 17:19:15 +0000

    Add helper functions for yappi profiling in notebooks

--- src/apd/aggregation/utils.py
+++ src/apd/aggregation/utils.py
@@ -1,4 +1,11 @@
+from __future__ import annotations
+
+import contextlib
+import importlib
+import io
 import math
+import os
+import typing as t
 
 
 # Set up mercator transform from https://wiki.openstreetmap.org/wiki/Mercator#Python
@@ -25,3 +32,40 @@ def merc_y(lat: float) -> float:
     ts = math.tan((math.pi / 2 - phi) / 2) / con
     y = 0 - r_major * math.log(ts)
     return y
+
+
+@contextlib.contextmanager
+def jupyter_page_file() -> t.Iterator[io.StringIO]:
+    from IPython.core import page
+
+    output = io.StringIO()
+    yield output
+    output.seek(0)
+    page.page(output.read())
+
+
+@contextlib.contextmanager
+def profile_with_yappi() -> t.Iterator[None]:
+    import yappi
+
+    yappi.clear_stats()
+    yappi.start()
+    yield None
+    yappi.stop()
+
+
+class YappiPackageFilter:
+    """ This object can be passed to yappi's modname filter to limit
+    by Python package rather than module filename"""
+
+    def __init__(self, package: str) -> None:
+        mod = importlib.import_module(package)
+        self.fn = mod.__file__
+        if self.fn.endswith("__init__.py"):
+            self.fn = os.path.dirname(self.fn)
+
+    def __eq__(self, other: object) -> t.Union[bool, NotImplemented]:
+        if isinstance(other, str):
+            return other.startswith(self.fn)
+        else:
+            return NotImplemented
