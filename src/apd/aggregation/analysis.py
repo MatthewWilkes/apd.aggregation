0be751b296ef5a8c1afa000963db5ebdfa1ef10b
Matthew Wilkes <git@matthewwilkes.name>
2020-04-28 11:38:20 +0100

    Improve reliability of deprecation warnings

--- src/apd/aggregation/analysis.py
+++ src/apd/aggregation/analysis.py
@@ -54,9 +54,7 @@ class Config(t.Generic[T_key, T_value]):
     def __post_init__(self, sensor_name: t.Optional[str] = None) -> None:
         if self.draw is None:
             self.draw = draw_date  # type: ignore
-        if self.get_data is None:
-            if sensor_name is None:
-                raise ValueError("You must specify either get_data or sensor_name")
+        if sensor_name is not None:
             warnings.warn(
                 DeprecationWarning(
                     f"The sensor_name parameter is deprecated. Please pass "
@@ -66,7 +64,10 @@ class Config(t.Generic[T_key, T_value]):
                 ),
                 stacklevel=3,
             )
-            self.get_data = get_one_sensor_by_deployment(sensor_name)
+            if self.get_data is None:
+                self.get_data = get_one_sensor_by_deployment(sensor_name)
+        if self.get_data is None:
+            raise ValueError("You must specify a get_data function")
 
 
 def draw_date(
